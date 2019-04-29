7a598fa30f67b4e1225cdce31c908a6906ee8e7c
Mattroi <trinhducnam1@gmail.com>
2019-04-29 16:53:25 +0700

    comment

--- server/routers/post.js
+++ server/routers/post.js
@@ -7,8 +7,8 @@ module.exports = function(app) {
 	app.get("/posts/:id", postController.show)
 	app.delete("/posts/:id", postController.remove)
 
-	// app.put("/posts/:id/add_comment", postController.createComment)
-	// app.put("/posts/:id/update_comment/:idComment", postController.updateComment)
+	app.put("/posts/:id/add_comment", postController.createComment)
+	app.put("/posts/:id/update_comment/:idComment", postController.updateComment)
 	app.put("/posts/:id/delete_comment/:idComment", postController.removeComment)
 	app.get("/posts/:id/index_comment", postController.indexComment)
 
